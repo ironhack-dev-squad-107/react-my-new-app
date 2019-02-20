96258e9b4e1f799c1399e52cfb4626c39de88b83
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2019-02-20 10:41:02 +0100

    Add conditional rendering examples

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,6 +2,20 @@ import React, { Component } from "react";
 
 import "./MovieList.css";
 
+function longRatingText(movie) {
+  if (movie.hasOscars && movie.imdbRating >= 9) {
+    return <p>🤯 Mindblowing film! Rating is {movie.imdbRating}.</p>;
+  } else if (movie.hasOscars && movie.imdbRating >= 7) {
+    return (
+      <p>
+        😮 Solid film! Has Oscars and a decent rating of {movie.imdbRating}.
+      </p>
+    );
+  } else {
+    return <p>Rating is {movie.imdbRating}.</p>;
+  }
+}
+
 class MovieList extends Component {
   // use constructor for ONLY when you need state
   constructor(props) {
@@ -9,13 +23,35 @@ class MovieList extends Component {
 
     // initial state of our component
     this.state = {
+      showOscarFilmsOnly: false,
       movieArray: [
-        { _id: "7g", title: "The Godfather", director: "Francis Coppola" },
-        { _id: "8h", title: "Star Wars", director: "George Lucas" },
+        {
+          _id: "7g",
+          title: "The Godfather",
+          director: "Francis Coppola",
+          imdbRating: 9.2,
+          hasOscars: true
+        },
+        {
+          _id: "8h",
+          title: "Star Wars",
+          director: "George Lucas",
+          imdbRating: 8.7,
+          hasOscars: true
+        },
         {
           _id: "9i",
           title: "The Shawshank Redemption",
-          director: "Frank Darabont"
+          director: "Frank Darabont",
+          imdbRating: 9.3,
+          hasOscars: false
+        },
+        {
+          _id: "10j",
+          title: "Glitter",
+          director: "Vondie Curtis-Hall",
+          imdbRating: 2.2,
+          hasOscars: false
         }
       ]
     };
@@ -31,23 +67,54 @@ class MovieList extends Component {
     this.setState({ movieArray: movies });
   }
 
+  toggleOscarFilter() {
+    const oldFilter = this.state.showOscarFilmsOnly;
+    // set the state to the opposite of the boolean value it had before
+    this.setState({ showOscarFilmsOnly: !oldFilter });
+  }
+
   render() {
-    const { movieArray } = this.state;
+    const { movieArray, showOscarFilmsOnly } = this.state;
 
     return (
       <section className="MovieList">
         <h2>Movie List Example</h2>
 
+        <button onClick={() => this.toggleOscarFilter()}>
+          Show {showOscarFilmsOnly ? "All Films" : "Oscar Winners Only"}
+        </button>
+
         <ul>
           {movieArray.map((oneMovie, index) => {
             return (
-              // add a UNIQUE key to the HTML tag you return in map()
-              // (this helps React be more efficient when updating the DOM)
-              <li key={oneMovie._id}>
-                <h3>{oneMovie.title}</h3>
-                <p>Directed by {oneMovie.director}</p>
-                <button onClick={() => this.deleteMovie(index)}>Delete</button>
-              </li>
+              // return the <li> of this movie if the filter is FALSE
+              // -OR- if the filter is TRUE and the movie has Oscars
+              (!showOscarFilmsOnly || oneMovie.hasOscars) && (
+                // add a UNIQUE key to the HTML tag you return in map()
+                // (this helps React be more efficient when updating the DOM)
+                <li key={oneMovie._id}>
+                  <h3>{oneMovie.title}</h3>
+                  <p>Directed by {oneMovie.director}</p>
+
+                  {/* if the movie has won oscars */}
+                  {oneMovie.hasOscars ? (
+                    <p>Oscar Winning Film 🏆</p>
+                  ) : (
+                    <p>Great film, but no Oscars. 😕</p>
+                  )}
+                  {/* CONDITION ? OPTION A : OPTION B */}
+
+                  {/* if the rating is less than 4 */}
+                  {oneMovie.imdbRating < 4 && <p>This movie is 💩</p>}
+                  {/* {CONDITION && CONTENT} */}
+
+                  {longRatingText(oneMovie)}
+
+                  <button onClick={() => this.deleteMovie(index)}>
+                    Delete
+                  </button>
+                </li>
+              )
             );
           })}
         </ul>
